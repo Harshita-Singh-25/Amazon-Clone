7434c006c9f4cc7c3d8e66d21efa51e180faed2c
Harshita Singh <2023.harshita.singh@ves.ac.in>
2025-01-04 18:25:24 +0530

    ADDED INITIAL STYLE FOR PAGE

---
+++ Amazon.js
@@ -0,0 +1,29 @@
+{/* <div class="product-container">
+                <div class="product-img-container">
+                    <img class="prod-img">
+                </div>
+                <div class="product-name">
+                    4
+                </div>
+                <div class="product-rating-container">
+                    <img class="product-rating-stars">
+                    <div class="product-rating-count"> 90</div>
+                </div>
+                <div class="product-price"></div>
+                <div class="product-quantity-container">
+                    <select>
+                        <option selected value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>    
+                </div>
+
+                <div class="product-spacer"></div>
+                <div class="added-to-cart">
+                    <button class="add-to-cart-button">
+                        Add to Cart
+                    </button>
+                </div>
+            </div> */}
